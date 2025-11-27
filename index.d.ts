e2a7c1d70e5c6d815f07c9d0c0dabc62410d2364
fangqijun <zfangqj@gmail.com>
2025-11-27 20:03:37 +0800

    更新ts接口

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,21 @@
-export = unmuteIosAudio;
-declare function unmuteIosAudio(): void;
+interface UnmuteIosAudioOptions {
+    /**
+     * reuse your context reference
+     */
+    context: AudioContext;
+    /**
+     * override internal ios detection    
+     * default: false
+     */
+    forceIos?: boolean;
+    /**
+     * run when user triggers an event
+     * default: true
+     */
+    whenEvent?: boolean;
+}
+declare function unmuteIosAudio(options: UnmuteIosAudioOptions): void;
+
+export {
+    unmuteIosAudio
+}
